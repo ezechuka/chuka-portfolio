8c4b6d4f8a27402b2bd6e76d6e672d54933e6051
ezechuka <ezechuka9@gmail.com>
2025-06-24 12:12:03 +0100

    feat: Implement contact section

---
+++ src/sections/Contact.tsx
@@ -0,0 +1,40 @@
+import grainImage from "@/assets/images/grain.jpg";
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+
+const Contact = () => {
+  return (
+    <div className="py-16 pt-12 lg:py-24 lg:pt-20">
+      <div className="container">
+        <div className="portfolio-theme-gradient relative z-0 overflow-hidden rounded-3xl px-10 py-8 text-center text-gray-900 md:text-left">
+          <div
+            className="absolute inset-0 -z-10 opacity-5"
+            style={{
+              backgroundImage: `url(${grainImage.src})`,
+            }}
+          />
+          <div className="flex flex-col items-center gap-8 md:flex-row md:gap-16">
+            <div>
+              <h2 className="font-serif text-2xl md:text-3xl">
+                Let&apos;s create something amazing together
+              </h2>
+              <p className="mt-2 text-sm md:text-base">
+                Ready to bring your next project to life? Let&apos;s connect and
+                discuss how I can help you achieve your goals.
+              </p>
+            </div>
+            <div>
+              <a href="https://www.google.com" target="_blank">
+                <button className="inline-flex h-12 w-max items-center gap-2 rounded-xl border border-gray-900 bg-gray-900 px-6 text-white">
+                  <span className="font-semibold">Contact Me</span>
+                  <ArrowUpRightIcon className="size-4" />
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
