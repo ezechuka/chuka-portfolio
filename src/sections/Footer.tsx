66a1aab4588ab1e065b8d639466fed0e13ab194c
ezechuka <ezechuka9@gmail.com>
2025-06-24 12:12:17 +0100

    feat: Implement footer section

---
+++ src/sections/Footer.tsx
@@ -0,0 +1,29 @@
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import { portfolioContent } from "@/utils/content";
+
+export const Footer = () => {
+  return (
+    <footer className="relative -z-10 overflow-x-clip">
+      <div className="absolute bottom-0 left-1/2 -z-10 h-[400px] w-[1600px] -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)]"></div>
+      <div className="container">
+        <div className="flex flex-col items-center gap-8 border-t border-white/15 py-6 text-sm md:flex-row md:justify-between">
+          <div className="text-white/40">
+            &copy; {`${new Date().getFullYear()}.`} All rights reserved.
+          </div>
+          <nav className="flex flex-col items-center gap-8 md:flex-row">
+            {portfolioContent.footerContent.footerLinks.map((link) => (
+              <a
+                href="#"
+                key={link.title}
+                className="inline-flex items-center gap-1.5"
+              >
+                <span className="font-semibold">{link.title}</span>
+                <ArrowUpRightIcon className="size-4" />
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
+};
